c3cfa97dd8d58d6cdde2a5e3ca24e33c883a7fa7
mochi8k <y.hkh.185@gmail.com>
2016-07-14 23:01:15 +0900

    ホストとポートのハードコードを解消

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -18,18 +19,27 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+
+	// htmlからリクエスト内容を参照できる.
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリのドレス")
+
+	// フラグを解釈
+	flag.Parse()
+
 	r := newRoom()
 
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	go r.run()
 
+	log.Println("Webサーバーを開始します。ポート: ", *addr)
+
 	// start Web Server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
