9e8d4f9a62d6ea5f0d716114b7ab2eed22f8e37c
mochi8k <y.hkh.185@gmail.com>
2016-07-17 01:41:55 +0900

    ログの無効化に対応

--- trace/tracer.go
+++ trace/tracer.go
@@ -10,10 +10,6 @@ type Tracer interface {
 	Trace(...interface{})
 }
 
-func New(w io.Writer) Tracer {
-	return &tracer{out: w}
-}
-
 type tracer struct {
 	out io.Writer
 }
@@ -21,4 +17,16 @@ type tracer struct {
 func (t *tracer) Trace(a ...interface{}) {
 	t.out.Write([]byte(fmt.Sprint(a...)))
 	t.out.Write([]byte("\n"))
+}
+
+type nilTracer struct{}
+
+func (t *nilTracer) Trace(a ...interface{}) {}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
+func Off() Tracer {
+	return &nilTracer{}
 }
