8b4a97680567a0179e3fa8c7b8ad4032abed1d96
mochi8k <y.hkh.185@gmail.com>
2016-07-22 07:43:27 +0900

    use http

--- main2.go
+++ main2.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Hello World")
+}
+
 func main2() {
-	fmt.Println("my main")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
