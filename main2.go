c6e4a72c688fedc0260a1ae23272d9c80e89770c
mochi8k <y.hkh.185@gmail.com>
2016-07-25 23:55:25 +0900

    update main2.go

--- main2.go
+++ main2.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"path/filepath"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello Go!")
+	tmpl, err := template.ParseFiles(filepath.Join("templates", "chat.html"))
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	err = tmpl.Execute(w, r)
+
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
+	fmt.Println("main")
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
