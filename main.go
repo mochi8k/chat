d8d4f45153b65cc25a8bcfdb3846458d446c9055
mochi8k <y.hkh.185@gmail.com>
2016-07-31 14:35:06 +0900

    各プロバイダーへの認証(OAuth2)を追加

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ import (
 	"sync"
 
 	"github.com/mochi8k/chat/trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 type templateHandler struct {
@@ -32,6 +36,26 @@ func main() {
 
 	// フラグを解釈
 	flag.Parse()
+
+	// set up gomniauth
+	gomniauth.SetSecurityKey("tan security key")
+	gomniauth.WithProviders(
+		facebook.New(
+			"1659131477747431",
+			"05d8695a82578f5bdb4e5bc58893d007",
+			"http://localhost:8080/auth/callback/facebook",
+		),
+		github.New(
+			"25799018a8946414a78b",
+			"978387a957b2c4571a5a002333bb6c7405d5c385",
+			"http://localhost:8080/auth/callback/github",
+		),
+		google.New(
+			"219682017207-pa5443eapqatgbd6j3klpg7eutfij626.apps.googleusercontent.com",
+			"FwDuL2xoeTCJxpi6uyXmQKgK",
+			"http://localhost:8080/auth/callback/google",
+		),
+	)
 
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
