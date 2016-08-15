9ea1629d1857e3a747ce3dabf240cc93cf049f1d
mochi8k <y.hkh.185@gmail.com>
2016-08-15 10:44:44 +0900

    add avatar_test

---
+++ avatar_test.go
@@ -0,0 +1,22 @@
+package main
+import "testing"
+
+func TestAuthAvatar(t *testing.T) {
+    var authAvatar AuthAvatar
+    client := new(client)
+    url, err := authAvatar.GetAvatarURL(client)
+    if err != ErrNoAvatarURL {
+      t.Error("値が存在しない場合、ErrNoAvatarURLを返却すべきです.")
+    }
+
+    testUrl := "http://url-to-avatar"
+    client.userData = map[string]interface{}{"avatar_url": testUrl}
+
+    url, err = authAvatar.GetAvatarURL(client)
+    if err != nil {
+      t.Error("値が存在する場合、エラーを返却すべきではありません.")
+    } else if url != testUrl {
+      t.Error("正しいURLを返却すべきです.")
+    }
+
+}
